43bbd673f00967b11f9e5932ca5873df70e63441
David Howell <david@davidjameshowell.com>
2022-05-24 03:00:01 +0000

    Change discord bot runtime

--- discord-bot.py
+++ discord-bot.py
@@ -1,16 +1,10 @@
 # bot.py
-import os
-
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = "Nzk5MjgxNDk0NDc2NDU1OTg3.YABS5g.2lmzECVlZv3vv6miVnUaKPQi2wI" # Discord Bot token for our user join bot
-
-client = discord.Client()
+bot = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-client.run(TOKEN)
+bot.run('MZ1yGvKTjE0rY0cV8i47CjAa.uRHQPq.Xb1Mk2nEhe-4iUcrGOuegj57zMC')
