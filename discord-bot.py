de6079eb91b7d31cb2cbed2618c91d3169c5bca7
David Howell <david@davidjameshowell.com>
2022-05-24 03:02:00 +0000

    Debug discord bot

--- discord-bot.py
+++ discord-bot.py
@@ -1,10 +1,20 @@
 # bot.py
-import discord
+import os
 
-bot = discord.Client()
+import discord
+from dotenv import load_dotenv
+
+# TESTING CREDENTIALS, REMOVE BEFORE PUSHING TO GITHUB
+# client_id=840643401846147428;
+# client_secret=iodDmFSUDXXirwgOVc15vSBKxDt8n-25;
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
 
-bot.run('MZ1yGvKTjE0rY0cV8i47CjAa.uRHQPq.Xb1Mk2nEhe-4iUcrGOuegj57zMC')
+client.run(TOKEN)
